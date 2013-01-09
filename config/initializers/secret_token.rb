b2f0f35e4cac031cfb8e403a23c612a979c667fd
shkvark <zvo1981@gmail.com>
2013-01-09 10:28:51 +0400

    Initial commit 2013-01-09

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PgApp::Application.config.secret_token = '5ec4f5562604cc44a18a44ae94c8c9711213512f4b5b7115f461cd81e5945bfaa74fd6c5e24847fb4b23b6585bfa2fab04b8e65ae51108343577b61602870380'
